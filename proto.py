a85d70ff1d9d7474ebb968de1e011d41702374c2
bouDeScotch <floblok180@gmail.com>
2022-09-22 22:56:31 +0200

    first commit (in this branch)

--- proto.py
+++ proto.py
@@ -1,12 +1,7 @@
 def mainLoop():
     mode = "non"
-    while type(mode) != int and mode not in (1, 2, "quit"):
+    while mode not in (1, 2, "quit"):
         mode = input("Which mode use ? \n1 - Flash card \n2 - Agenda\n\n")
-        try:
-            mode = int(mode)
-        except:
-            if mode == "quit":
-                break
     
     if (mode == 1):
         flashCard()
@@ -22,7 +17,25 @@ def quiting():
     print("Currently stopping the program")
 
 def flashCard():
-    print("Mode en cours de construction")
+
+    def creation():
+        pass
+
+    def studying():
+        pass
+
+    
+    action = "non"
+    while action not in (1, 2, "quit"):
+        action = input("Do you want to : \n1 - Create new flash cards \n2 - Study the existing flash cards")
+    
+    if (action == 1):
+        creation()
+    elif (action == 2):
+        studying()
+    
+    if (action != "quit"):
+        flashCard()
 
 def agenda():
     print("Mode en cours de construction")
